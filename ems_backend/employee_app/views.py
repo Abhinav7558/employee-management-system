76e3092b2a74e23c5ca913b702d46648e69c8d07
Abhinav <abhinavdas2301@gmail.com>
2025-07-06 22:22:33 +0530

    feat: customize EmployeeViewSet responses for get and list actions

--- ems_backend/employee_app/views.py
+++ ems_backend/employee_app/views.py
@@ -1,8 +1,13 @@
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import viewsets, status
 from rest_framework import viewsets, permissions
-from .models import Employee
-from .serializers import EmployeeSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.exceptions import NotFound
+
+from .models import Employee
+from .serializers import EmployeeSerializer
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
@@ -12,6 +17,26 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ["form_template", "is_active"]
     search_fields = ["field_values__field_value"]
+
+    def get_object(self):
+        try:
+            return super().get_object()
+        except Http404:
+            raise NotFound(detail={"message": "No employees found."})
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if not queryset.exists():
+            return Response({"message": "No Employees available"}, status=status.HTTP_200_OK)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
